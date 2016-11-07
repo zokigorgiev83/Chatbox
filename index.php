d1af3d5a50e71c7e471945876c60d3d7b73bf546
zokigorgiev83 <zokigorgiev83@gmail.com>
2016-11-08 00:39:29 +0100

    "HTML and unfinished CSS"

--- index.php
+++ index.php
@@ -2,11 +2,34 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Shoutbox</title>
+        <link href="css/master.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
     </head>
     <body>
-        <?php
-        echo 'Hello, world!';
-        ?>
+        <div id="container">
+            <header>
+                <h1>Shoutbox</h1>
+            </header>
+            
+            <main id="shouts">
+                <ul>
+                    <li></li>
+                </ul>
+            </main>
+            
+            <footer>
+                <form>
+                    <label>Name</label>
+                    <input type="text" id="name">
+                    <label>Shout Text</label>
+                    <input type="text" id="shout">
+                    <input type="submit" id="submit" value="SHOUT!">
+                </form>
+            </footer>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
